c315927cc91ea7272c8088175bee0702cd3f0175
Leandro <leandro2791@hotmail.com>
2024-12-28 15:17:50 -0300

    feito a responsividade e ajustado o firebase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,13 +1,16 @@
+import dotenv from 'dotenv';
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+dotenv.config();
+
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
